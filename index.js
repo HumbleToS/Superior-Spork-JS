70246da849830b87fd162d00cb0242d4e26dcb7c
Humble <70359789+HumbleToS@users.noreply.github.com>
2022-08-12 23:10:43 -0600

    Added a check if altconfig.json exists If it does not exist it uses config.json this is so i have my own config Added allowedMentions Created on guild join and leave events for status

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const { Client, Collection, GatewayIntentBits } = require('discord.js')
-const { token } = require('./config.json');
+const { Client, Collection, GatewayIntentBits, ActivityType } = require('discord.js')
 const fs = require('node:fs');
 const path = require('node:path');
 
-const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers]});
+const { token } = require(fs.existsSync('./altconfig.json') ? './altconfig.json' : './config.json');
+
+const client = new Client({
+    allowedMentions: {
+        parse: ['users'],
+        roles: false,
+        everyone: false,
+        repliedUser: true
+    },
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers]
+});
 
 client.commands = new Collection();
+client.start_time = Math.round(new Date().getTime() / 1000);
 
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
@@ -14,10 +24,39 @@ for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
     client.commands.set(command.data.name, command);
+    console.log(`Loaded command ${command.data.name}`);
 }
 
 client.once('ready', () => {
     console.log("Bot has started");
+
+    client.user.setPresence({ 
+        activities: [{ 
+            name: ` over ${client.guilds.cache.size} servers`, 
+            type: ActivityType.Watching 
+        }], 
+        status: 'dnd' 
+    });
+});
+
+client.on("guildCreate", async(guild) => {
+    client.user.setPresence({ 
+        activities: [{ 
+            name: ` over ${client.guilds.cache.size} servers`, 
+            type: ActivityType.Watching 
+        }], 
+        status: 'dnd' 
+    });
+});
+
+client.on("guildDelete", async(guild) => {
+    client.user.setPresence({ 
+        activities: [{ 
+            name: ` over ${client.guilds.cache.size} servers`, 
+            type: ActivityType.Watching 
+        }], 
+        status: 'dnd' 
+    });
 });
 
 client.on('interactionCreate', async interaction => {
