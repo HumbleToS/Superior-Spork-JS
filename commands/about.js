b80d9c64a67cd08b9a6b7175426c3dbc7fc7b997
Humble <70359789+HumbleToS@users.noreply.github.com>
2022-08-12 23:13:03 -0600

    Now shows Ping and API Latency in about command

--- commands/about.js
+++ commands/about.js
@@ -5,17 +5,19 @@ module.exports = {
         .setName('about')
         .setDescription('General information about the bot!'),
     async execute(interaction) {
+        start = new Date().getTime();
+        await interaction.channel.sendTyping();
+        end = new Date().getTime();
         const dev = interaction.client.users.cache.get('739219467455823921');
         const aboutEmbed = new EmbedBuilder()
             .setColor(0xffffff)
             .setTitle('Superior Spork')
-            .setDescription('This is essentially `Superior Spork#0830` but in JavaScript!')
+            .setDescription(`This is essentially \`Superior Spork#0830\` but in JavaScript!\nBot was started on <t:${interaction.client.start_time}:f>`)
             .addFields([
                 { name: 'Developer', value: `${dev.tag}` },
-                { name: 'Latency', value: `${interaction.client.ws.ping}ms` }
+                { name: 'Bot Latency', value: `Latency: \`${end-start}ms\`\nAPI latency: \`${interaction.client.ws.ping}ms\`` }
             ])
             .setTimestamp();
-
         await interaction.reply({ embeds: [aboutEmbed] });
     },
 };
