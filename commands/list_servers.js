d42f625c88dee5db47440b395325a8554d9e1618
Humble <70359789+HumbleToS@users.noreply.github.com>
2022-09-15 20:06:56 -0600

    stupid stupid stupid stupid stupid stupid stupid stupid stupid stupid stupid stupid stupid stupid stupid

--- commands/list_servers.js
+++ commands/list_servers.js
@@ -1,10 +1,17 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, codeBlock, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ls')
         .setDescription('List the servers the bot is in'),
     async execute(interaction) {
-        await interaction.reply(`test ${interaction.client.guilds.cache.size} servers`)
+        if (interaction.user.id != '739219467455823921') {
+            return await interaction.reply({content: 'That command is owner only!', ephemeral: true})
+        };
+        let guilds = [];
+        interaction.client.guilds.cache.forEach(guild => {
+            guilds.push(`Guild Name: ${guild.name} | Guild ID: ${guild.id} | Member Count: ${guild.memberCount}`)
+        });
+        await interaction.reply(`${codeBlock(guilds.join('\n'))}`);
     },
 };
